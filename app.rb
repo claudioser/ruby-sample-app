e2f7c5f864e8c019061e6691e8c71a7f2be5b8c3
Claudio Serrano <53047113+claudioser@users.noreply.github.com>
2020-05-19 10:58:40 +0200

    ref
    - red colour

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  '<h1 style="color: green;">SUSE Cloud Application Platform Demo on Azure - Microsoft !</h1>'
+  '<h1 style="color: red;">SUSE Cloud Application Platform Demo on Azure - Microsoft !</h1>'
 end
