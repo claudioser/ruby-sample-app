ac5ad9cd28a9566ade71f4e54386b9fa110b396b
Claudio Serrano <53047113+claudioser@users.noreply.github.com>
2020-06-03 15:34:14 +0200

    vv
    vv

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  '<h1 style="color: green;">SUSE Cloud Application Platform Demo - Azure - Microsoft Webinar !</h1>'
+  '<h1 style="color: blue;">SUSE Cloud Application Platform Demo - Azure - Microsoft Webinar !</h1>'
 end
