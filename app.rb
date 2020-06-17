2bed40fa9cc0930235d80adc5a35e95275015ab5
Claudio Serrano <53047113+claudioser@users.noreply.github.com>
2020-06-17 16:42:32 +0200

    AppProd
    Initial Version

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  '<h1 style="color: red;">SUSE Cloud Application Platform Demo on Azure for  Microsoft / Adfinis Webinar !</h1>'
+  '<h1 style="color: red;">SUSE Cloud Application Platform Demo on Azure for Microsoft / Adfinis Webinar !</h1>'
 end
