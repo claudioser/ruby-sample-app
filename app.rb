dcc1bd757bdfe69b10d0f8efadd98eea9729e648
Claudio Serrano <53047113+claudioser@users.noreply.github.com>
2020-08-27 12:16:56 +0200

    V1
    App - OL

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  '<h1 style="color: red;">SUSE Cloud Application Platform Demo - v1 app !</h1>'
+  '<h1 style="color: red;">SUSE Cloud Application Platform Demo - v1 app  !</h1>'
 end
